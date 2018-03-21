0508bff26c0e9002fc0b048f6fe9d8d0ad9520b5
mufichmj <mufichmj@rose-hulman.edu>
2018-03-20 22:14:20 -0400

    problem 1

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Mariah Mufich.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -119,7 +119,9 @@ def problem2(line1, line2, thickness, win):
     #          Tests have been written for you (above).
     #   See the IMPORTANT NOTE just above the DEF line above.
     # -------------------------------------------------------------------------
-
+    # Step One
+    line1.attach_to(win)
+    line2.attach_to(win)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
